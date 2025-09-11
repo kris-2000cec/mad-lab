ec6fe70ea5ba7c7e74e9e502962d92283b244bfb
kris-2000cec <chn24mc015@ceconline.edu>
2025-09-11 15:38:27 +0530

    Create activityOne.java

---
+++ cycle3/menu_expt09/activityOne.java
@@ -0,0 +1,13 @@
+package com.example.menudriven_expt09;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+// This is the first destination activity.
+public class activityone extends AppCompatActivity {
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityone); // Display the layout for this activity.
+    }
+}
