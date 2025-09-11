be5ea064e7cb64c272d9feadb460f6035fded9aa
kris-2000cec <chn24mc015@ceconline.edu>
2025-09-11 15:40:48 +0530

    Create activityTwo.java

---
+++ cycle3/menu_expt09/activityTwo.java
@@ -0,0 +1,14 @@
+package com.example.menudriven_expt09;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+// This is the second destination activity.
+public class activityTwo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two); // Display the layout for this activity.
+    }
+}
