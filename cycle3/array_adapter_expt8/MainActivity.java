eb427b246b3c02c48cb2b1da148de292100f7163
kris-2000cec <chn24mc015@ceconline.edu>
2025-09-11 14:21:58 +0530

    Create MainActivity.java

---
+++ cycle3/array_adapter_expt8/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.arrayadapterexpt08;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Grape", "Kiwi", "Lemon", "Mango",
+                "Orange", "Peach"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, fruits);
+        listView.setAdapter(adapter);
+    }
+}
